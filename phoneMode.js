c9c474ee78e9b78ecfe0f9c18716cd07f5fe47e2
B2060B0202 <B2060B0202@kginter.net>
2020-07-07 10:18:07 +0300

    safari updates

--- phoneMode.js
+++ phoneMode.js
@@ -1,6 +1,22 @@
 // דף קולי שמסדר בעזרת השם את הבעיה בטלפונים שהמסך יהיה רק לרוחב
 $(function () {
     if ('ontouchstart' in document) {
-        screen.orientation.lock("landscape");    
+        var promise = document.body.requestFullscreen();
+        promise.then(() => {
+            screen.orientation.lock("landscape");
+        }, () => {
+            window.matchMedia("(orientation: portrait) or (orientation: portrait-primary)").addListener(function() {
+                if (!this.matches) {
+                    document.documentElement.style.transform = `rotateY(-90deg)`;
+                    document.documentElement.style.width = "100vh";
+                    document.documentElement.style.height = "100vw";
+                    e.preventDefault();
+                }else {
+                    document.documentElement.style.transform = "";
+                    document.documentElement.style.width = "";
+                    document.documentElement.style.height = "";
+                }
+            });
+        })
     }
 });
