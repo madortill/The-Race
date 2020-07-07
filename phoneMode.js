b149c1aa14ecad6247be3d6cbcbe9e783c3b28b5
B2060B0202 <B2060B0202@kginter.net>
2020-07-07 15:10:50 +0300

    fixed first invocation of orient

--- phoneMode.js
+++ phoneMode.js
@@ -7,27 +7,29 @@ if ('serviceWorker' in navigator && location.hostname !== 'localhost') {
 
 // דף קולי שמסדר בעזרת השם את הבעיה בטלפונים שהמסך יהיה רק לרוחב
 $(function () {
-    if ('ontouchstart' in document)
-        window.matchMedia("(orientation: portrait)").addListener(oriente);
-    function oriente(e) {
-        if (this.matches) {
-            document.documentElement.style.transform = "translate(calc(-100% + 100vw)) rotateZ(90deg) translate(100%)";
-            document.documentElement.style.transformOrigin = "top right";
-            document.documentElement.style.left = "0";
-            document.documentElement.style.position = "absolute";
-            document.documentElement.style.width = `${window.innerHeight}px`;
-            document.documentElement.style.height = `${window.innerWidth}px`;
-            if(window.onportrait) onportrait();
-            e.preventDefault();
-        } else {
-            if(window.onlandscape) onlandscape();
-            document.documentElement.style.transform = "";
-            document.documentElement.style.transformOrigin = "";
-            document.documentElement.style.left = "";
-            document.documentElement.style.position = "";
-            document.documentElement.style.width = ``;
-            document.documentElement.style.height = ``;
+    if ('ontouchstart' in document) {
+        let media = window.matchMedia("(orientation: portrait)");
+        media.addListener(oriente);
+        function oriente(e) {
+            if (this.matches) {
+                document.documentElement.style.transform = "translate(calc(-100% + 100vw)) rotateZ(90deg) translate(100%)";
+                document.documentElement.style.transformOrigin = "top right";
+                document.documentElement.style.left = "0";
+                document.documentElement.style.position = "absolute";
+                document.documentElement.style.width = `${window.innerHeight}px`;
+                document.documentElement.style.height = `${window.innerWidth}px`;
+                if(window.onportrait) onportrait();
+                e.preventDefault();
+            } else {
+                if(window.onlandscape) onlandscape();
+                document.documentElement.style.transform = "";
+                document.documentElement.style.transformOrigin = "";
+                document.documentElement.style.left = "";
+                document.documentElement.style.position = "";
+                document.documentElement.style.width = ``;
+                document.documentElement.style.height = ``;
+            }
         }
+        oriente.apply(media, new Event("orientationchange"));
     }
-    oriente();
 });
